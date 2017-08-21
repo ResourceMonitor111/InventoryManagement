77d3d11b1dc049a9c799bc7eda88222685bcca28
AkimovsI <31030332+AkimovsI@users.noreply.github.com>
2017-08-21 12:09:10 +0300

    updated repo

--- zxing-sample/src/main/java/model/Repo.java
+++ zxing-sample/src/main/java/model/Repo.java
@@ -1,20 +1,27 @@
-package model;
+package data;
 import android.content.Context;
-/**
- * Created by kirillsuvorov on 18/08/2017.
- */
+
 
 public class Repo {
-    DatabaseHelper db;
+	DatabaseHelper db;
 
-    public RepoUsers Products;
+	public RepoUsers Products;
+	public RepoUsers BillsOfMaterialsORM;
+//	public RepoUsers Clerk;
+//	public RepoUsers ShopTransaction;
+//	public RepoUsers ProductTransaction;
 
-    public Repo(Context context)
-    {
-        DatabaseManager<DatabaseHelper> manager = new DatabaseManager<DatabaseHelper>();
-        db = manager.getHelper(context);
+	public Repo(Context context)
+	{
+		DatabaseManager<DatabaseHelper> manager = new DatabaseManager<DatabaseHelper>();
+		db = manager.getHelper(context);
 
-        Products = new RepoUsers(db);
 
-    }
+		Products = new RepoUsers(db);
+		BillsOfMaterialsORM = new RepoUsers(db);
+//		Clerk = new RepoUsers(db);
+//		ShopTransaction = new RepoUsers(db);
+//		ProductTransaction = new RepoUsers(db);
+
+	}
 }
