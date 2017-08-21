8434078c8322b78245d106f00d45e088c9fe37d5
AkimovsI <31030332+AkimovsI@users.noreply.github.com>
2017-08-21 11:26:27 +0300

    RepoUsers with BillOfMaterials added

--- zxing-sample/src/main/java/model/RepoUsers.java
+++ zxing-sample/src/main/java/model/RepoUsers.java
@@ -1,110 +1,196 @@
-package model;
-
-/**
- * Created by kirillsuvorov on 18/08/2017.
- */
+package data;
 
 import java.sql.SQLException;
 import java.util.List;
 
+import model.BillOfMaterialsORM;
 import model.Product;
-//import com.j256.ormlite.dao;
-import com.j256.ormlite.dao.BaseDaoImpl;
-import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.stmt.QueryBuilder;
 
-    public class RepoUsers {
+public class RepoUsers {
 
-        Dao<Product, Integer> ProductDao = null;
+    Dao<BillOfMaterialsORM, Integer> BillOfMaterialsORMDao = null;
+    Dao<Product, Integer> ProductDao = null;
 
-        /*public Dao<Product, String> getProductDao() throws SQLException {
-            if (ProductDao == null) {
-                ProductDao = BaseDaoImpl.createDao(getConnectionSource(), Product.class);
-            }
-            return ProductDao;
-        }*/
-
-        public RepoUsers(DatabaseHelper db)
-        {
-            try {
-                ProductDao = db.getProductDao();
-            } catch (SQLException e) {
-                // TODO: Exception Handling
-                e.printStackTrace();
-            }
+    public RepoUsers(DatabaseHelper db)
+    {
+        try {
+            ProductDao = db.getProductDao();
+            BillOfMaterialsORMDao = db.getBillOfMaterialsORMDao();
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
         }
-
-        public int create(Product Product)
-        {
-            try {
-                return ProductDao.create(Product);
-            } catch (SQLException e) {
-                // TODO: Exception Handling
-                e.printStackTrace();
-            }
-            return 0;
-        }
-        public int update(Product Product)
-        {
-            try {
-                return ProductDao.update(Product);
-            } catch (SQLException e) {
-                // TODO: Exception Handling
-                e.printStackTrace();
-            }
-            return 0;
-        }
-        public int delete(Product Product)
-        {
-            try {
-                return ProductDao.delete(Product);
-            } catch (SQLException e) {
-                // TODO: Exception Handling
-                e.printStackTrace();
-            }
-            return 0;
-        }
-        public Product getByProductname(String Productname)
-        {
-            try {
-                QueryBuilder<Product, Integer> qb = ProductDao.queryBuilder();
-
-                qb.where().eq("name", Productname);
-
-                PreparedQuery<Product> pq = qb.prepare();
-                return ProductDao.queryForFirst(pq);
-            } catch (SQLException e) {
-                // TODO: Exception Handling
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        public Product getProductByBarcode(String barcode){
-            try {
-                QueryBuilder<Product, Integer> qb = ProductDao.queryBuilder();
-
-                qb.where().eq("barcode", barcode);
-
-                PreparedQuery<Product> pq = qb.prepare();
-                return ProductDao.queryForFirst(pq);
-            } catch (SQLException e) {
-                // TODO: Exception Handling
-                e.printStackTrace();
-            }
-            return null;
-        }
-        public List<Product> getAll()
-        {
-            try {
-                return ProductDao.queryForAll();
-            } catch (SQLException e) {
-                // TODO: Exception Handling
-                e.printStackTrace();
-            }
-            return null;
-        }
-
     }
+
+    public int create(Product Product)
+    {
+        try {
+            return ProductDao.create(Product);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int update(Product Product)
+    {
+        try {
+            return ProductDao.update(Product);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public int delete(Product Product)
+    {
+        try {
+            return ProductDao.delete(Product);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public Product getByProductname(String Productname)
+    {
+        try {
+            QueryBuilder<Product, Integer> qb = ProductDao.queryBuilder();
+
+            qb.where().eq("name", Productname);
+
+            PreparedQuery<Product> pq = qb.prepare();
+            return ProductDao.queryForFirst(pq);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public Product getProductByBarcode(String barcode){
+        try {
+            QueryBuilder<Product, Integer> qb = ProductDao.queryBuilder();
+
+            qb.where().eq("barcode", barcode);
+
+            PreparedQuery<Product> pq = qb.prepare();
+            return ProductDao.queryForFirst(pq);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public List<Product> getAllProducts()
+    {
+        try {
+            return ProductDao.queryForAll();
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /*
+    BillOfMaterials Repo
+     */
+
+    public int create(BillOfMaterialsORM BillOfMaterialsORM)
+    {
+        try {
+            return BillOfMaterialsORMDao.create(BillOfMaterialsORM);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    public int update(BillOfMaterialsORM BillOfMaterialsORM)
+    {
+        try {
+            return BillOfMaterialsORMDao.update(BillOfMaterialsORM);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public int delete(BillOfMaterialsORM BillOfMaterialsORM)
+    {
+        try {
+            return BillOfMaterialsORMDao.delete(BillOfMaterialsORM);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public BillOfMaterialsORM getBillByProductId(String productId)
+    {
+        try {
+            QueryBuilder<BillOfMaterialsORM, Integer> qb = BillOfMaterialsORMDao.queryBuilder();
+
+            qb.where().eq("productId", productId);
+
+            PreparedQuery<BillOfMaterialsORM> pq = qb.prepare();
+            return BillOfMaterialsORMDao.queryForFirst(pq);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public BillOfMaterialsORM getBillByDelivery (String deliveredBy){
+        try {
+            QueryBuilder<BillOfMaterialsORM, Integer> qb = BillOfMaterialsORMDao.queryBuilder();
+
+            qb.where().eq("deliveredBy", deliveredBy);
+
+            PreparedQuery<BillOfMaterialsORM> pq = qb.prepare();
+            return BillOfMaterialsORMDao.queryForFirst(pq);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public BillOfMaterialsORM getBillByDate (String datetimeAdded){
+        try {
+            QueryBuilder<BillOfMaterialsORM, Integer> qb = BillOfMaterialsORMDao.queryBuilder();
+
+            qb.where().eq("datetimeAdded", datetimeAdded);
+
+            PreparedQuery<BillOfMaterialsORM> pq = qb.prepare();
+            return BillOfMaterialsORMDao.queryForFirst(pq);
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public List<BillOfMaterialsORM> getAllBills()
+    {
+        try {
+            return BillOfMaterialsORMDao.queryForAll();
+        } catch (SQLException e) {
+            // TODO: Exception Handling
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
